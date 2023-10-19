b2ed476a1b69c27102f5eabfc8975ba98619c17d
Samuel <forfemi@gmail.com>
2023-10-19 10:03:13 +0100

    to-do-app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <todoPage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import todoPage from './components/todoPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todoPage,
+  }
+}
+</script>
+
+<style>
+
+</style>
